ebc4895f2e7df73de831f4e9c85aa1b7f6dc098a
Guilherme De Jesus <guilhermeadenilson16@gmail.com>
2022-08-27 14:23:05 -0300

    Adicionando apresentação do Membro 2

--- membro2.py
+++ membro2.py
@@ -1,2 +1,7 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return '''Olá, eu sou o \033[1mGuilherme\033[0m e tenho \033[1m20 anos\033[0m :D
+   Curto muito jogar, principalmente jogos de RPG e corrida, ler, assistir séries, filmes, animes (qualquer coisa).
+   Embora não seja um hobbie, tenho muito interesse por vôlei.
+   Antes do 1º semestre, nunca tive contato com programação. Contudo, consegui aprender o básico sobre python em POO1, além
+   de ter buscado entender um pouco sobre SQL.
+   '''
