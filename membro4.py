da4ebfc49c68d0f8896b442beb0ff8ee93b54815
Ayeloo <augustomborsoi@gmail.com>
2022-08-27 14:02:20 -0300

    Adicionando apresentação do Membro 4

--- membro4.py
+++ membro4.py
@@ -1,1 +1,3 @@
-
+def hello():
+	return '''Olá, meu nome é Augusto e tenho 19 anos. Meus hobbies são ilustração digital, level design e leitura. Tive experiência prévia com o phyton em POO1, e já usei linguagens como C++,
+    C# e um pouco de CSS. Tenho grande interesse em Programação OO, especialmente em python, pois quero um dia publicar um jogo feito usando a engine Godot.'''
