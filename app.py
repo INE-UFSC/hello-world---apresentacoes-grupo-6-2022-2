338e801df6be9412b768997c49081f8bf36c6f9e
memataexe <exe@cliente-150-162-149-81.wireless.ufsc.br>
2022-08-30 09:53:59 -0300

    Ajuste de indexação

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import membro2 as m2
 import membro3 as m3
 import membro4 as m4
 
+
 print(m1.hello())
 print(m2.hello())
 print(m3.hello())
--- membro1.py
+++ membro1.py
@@ -1,5 +1,7 @@
 def hello():
-	return '''Olá, meu nome é Diana, mas geralmente me chamam de Exe. 
-	Tenho 18 anos e gosto muito de jogar, desenhar e criar coisas no tempo livre. 
-	Eu gosto muito de PixelArt, é de longe meu estilo de arte favorito, e fiquei muito contente com o tema do projeto final da disciplina. 
-	Já programei um pouco em Python, na disciplina de POO1 e algumas outras linguagens, como C# e JS estou me dedicando pra por em prática fora da faculdade.'''
+	return '''\nOlá, meu nome é Diana, mas geralmente me chamam de Exe. 
+Tenho 18 anos e gosto muito de jogar, desenhar e criar coisas no tempo livre. 
+Eu gosto muito de PixelArt, é de longe meu estilo de arte favorito, e fiquei 
+muito contente com o tema do projeto final da disciplina.
+Já programei um pouco em Python, na disciplina de POO1 e algumas outras linguagens, como C# e JS estou me
+dedicando pra por em prática fora da faculdade.\n'''
